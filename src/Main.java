8ed091dcb9d4131a882fc67c06974930a4f9995b
Globa Alexandru <globa.alexandru02@gmail.com>
2024-10-02 16:44:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+enum An {
+    SEPTEMBRIE, OCTOMBRIE, NOIEMBRIE, DECEMBRIE, IANUARIE, FEBRUARUE, MARTIE, APRILIE, MAI,IUNIE,IULIE,AUGUST;
+
+}
+public class Main {
+    public static void main (String[] args) {
+ System.out.println(An.SEPTEMBRIE);
+ System.out.println(An.OCTOMBRIE);
+ System.out.println(An.NOIEMBRIE);
+ System.out.println(An.DECEMBRIE);
+ System.out.println(An.IANUARIE);
+ System.out.println(An.FEBRUARUE);
+ System.out.println(An.MARTIE);
+ System.out.println(An.APRILIE);
+ System.out.println(An.MAI);
+        }
+    }
